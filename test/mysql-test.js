29f00977192b0fb221a6eefc2c4da32613c8eccc
gessinger-hj <gessinger.hj@gmail.com>
2014-05-26 20:22:13 +0200

    mysql connector

--- test/mysql-test.js
+++ test/mysql-test.js
@@ -1,26 +1,24 @@
-var mysql =  require('mysql');
+var mysql =  require('mysql2');
+var T = require ( "Tango" ) ;
+  var dburl = T.getProperty ( "dburl", "mysql://root:@localhost/cdcol" ) ;
 
-console.log ( "1 --------------------------" ) ;
-  var connection =  mysql.createConnection({
-  	host : "localhost",
-  	user : "root",
-  	password: ""
-  });
-console.log ( "2 --------------------------" ) ;
+  var connection =  mysql.createConnection ( dburl ) ;
   connection.connect();
-console.log ( "2a --------------------------" ) ;
   connection.query( "use cdcol" );
-console.log ( "3 --------------------------" ) ;
 var strQuery = "select * from cds";	
-  
-  connection.query( strQuery, function(err, rows){
+
+connection.query( strQuery, function(err, rows, fields ){
+    // console.log ( fields ) ;
   	if(err)	{
   		throw err;
   	}else{
   		for ( var i = 0 ; i < rows.length ; i++ )
   		{
-  		console.log( rows[i] );
+  		// console.log( rows[i] );
   		}
   	}
   	connection.end() ;
   });
+q.on('end', function(result) {
+        console.log("(end)");
+    });
