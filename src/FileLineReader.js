a66ec1c13f14299b453e96a3d1a8ec6a4c8aaa9a
Hans-Jürgen Gessinger <gess@alghero.fritz.box>
2017-05-24 21:53:03 +0200

    move File.js to gbase

--- src/FileLineReader.js
+++ src/FileLineReader.js
@@ -8,6 +8,7 @@ var LineReader = require ( './LineReader' ) ;
  */
 var FileLineReader = function ( fileName )
 {
+	this.fileName = fileName ;
 	var options =
 	{
 		flags: 'r'
@@ -20,4 +21,47 @@ var FileLineReader = function ( fileName )
 }
 util.inherits ( FileLineReader, LineReader ) ;
 
+/**
+ * Description
+ * @param {} callback
+ * @return lr
+ */
+FileLineReader.prototype.lines = function ( callback )
+{
+	if ( callback )
+	{
+		var thiz = this ;
+		this.on ( "end", function()
+		{
+			callback.call ( thiz, null ) ;
+		});
+		this.on ( "line", function ( line )
+		{
+			callback.call ( thiz, line ) ;
+		});
+	}
+	return this ;
+};
 module.exports = FileLineReader ;
+if ( require.main === module )
+{
+ 	new FileLineReader ( "../package.json" ).lines ( function onarray(line)
+ 	{
+ console.log ( "line=" + line ) ;
+ 	});
+// 	var start = 0 ;
+// 	var end = -1 ;
+// 	var file = new File ( "x.log" ) ;
+// 	function read()
+// 	{
+// 		var opt = {} ;
+// 		opt.start = start ;
+// 		opt.end = file.length() ;
+// 		var str = fs.createReadStream ( file.path, opt ) ;
+// 		str.on ( "data", function ondata(data)
+// 		{
+// console.log ( "data=" + data ) ;
+// 		} ) ;
+// 		str.on ( "end", ) ;
+// 	}
+}
