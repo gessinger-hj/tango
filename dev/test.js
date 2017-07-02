4abaaee17492ddbf662164fff994842f509936a3
Hans-Jürgen Gessinger <gess@alghero.fritz.box>
2017-07-02 20:20:31 +0200

    next step 2

--- dev/test.js
+++ dev/test.js
@@ -1,5 +1,6 @@
 var T = require ( "tango" ) ;
-
+fetch = require('node-fetch') ;
+var util = require('util');
 // var t1 = "%NODE_TEST%-%GEPARD_LOG%" ;
 var t1 = "%GEPARD_LOG%" ;
 console.log ( "t1=" + t1 ) ;
@@ -8,7 +9,11 @@ console.log ( "t2=" + t2 ) ;
 
 async function get_ip() {
 	var response = await fetch("http://ipecho.net/plain");
-	var html = await response.text();
-	return html;
+	return  response.text();
 }
-
+var ip = get_ip() ;
+console.log ( "get_ip()=" + ip.then(function xx (a, b )
+	{
+		console.log(a);
+		console.log(b);
+	}) ) ;
